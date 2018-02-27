c18ac7f79c05cb2fdbd29c9b9cd75dd966174d63
chris.bridges <christopher_bridges@icloud.com>
2018-02-27 13:42:20 -0800

    cross-origin problems

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const GOOGLE_DISTANCE_MATRIX_ENDPOINT = 'https://maps.googleapis.com/maps/api/distancematrix/json';
 const GOOGLE_DISTANCE_MATRIX_API_KEY = 'AIzaSyA_p-CBKHLPbnsXnn6a3M-e25BxffzKBYM';
 
-const params = {
-	origins: 'San Francisco, California, USA',
-	destinations: 'Los Angeles, California, USA',
-	key: GOOGLE_DISTANCE_MATRIX_API_KEY,
-	language: 'en',
-	units: 'imperial'
-};
 
-let data = $.getJSON(GOOGLE_DISTANCE_MATRIX_ENDPOINT, params);
-console.log(data);
+function getData () {
+  const params = {
+  	origins: 'San Francisco, California, USA',
+  	destinations: 'Los Angeles, California, USA',
+  	key: GOOGLE_DISTANCE_MATRIX_API_KEY,
+  	language: 'en',
+  	units: 'imperial',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  };
+
+  let data = $.getJSON(GOOGLE_DISTANCE_MATRIX_ENDPOINT, params);
+  console.log(data);
+}
+
+$(getData);
+
+// THIS ONE ANY BETTER? - https://developers.google.com/maps/documentation/javascript/get-api-key
