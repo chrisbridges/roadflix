61746e5716a07be8d813bdadae149e1276d1547c
chris.bridges <christopher_bridges@icloud.com>
2018-02-28 21:25:20 -0800

    movies are showing properly now for both initial load and when scrolling with next

--- app.js
+++ app.js
@@ -30,57 +30,73 @@ function getDistance() {
 const THE_MOVIE_DATABASE_ENDPOINT = 'https://api.themoviedb.org/3/movie/popular'; // for popular movies
 const THE_MOVIE_DATABASE_KEY = 'f852305411e85c5520c80f92853fd711';
 const THE_MOVIE_DATABASE_IMAGE_BASEURL = 'https://image.tmdb.org/t/p';
-const THE_MOVIE_DATABASE_IMAGE_SIZE = '/w500';
+const THE_MOVIE_DATABASE_IMAGE_SIZE = '/w185';
 const popularMovies = [];
+const resultsPerPage = 5;
+let start = 0;
+let end = resultsPerPage;
+let pageNumber = 1;
+const addMovieButton = `<button class="add-movie">Add to List</button>`;
 
-function retrieveMovies () {
+function retrieveFirstTwentyMovies () {
+  $('.load-movies').on('click', function() {
+    retrieveMovies(pageNumber);
+    $('.load-movies').hide();
+    $('.prev-button').show();
+    $('.next-button').show();
+  });
+}
+
+function retrieveMovies (pageNumber) {
   const params = {
     api_key: THE_MOVIE_DATABASE_KEY,
     language: 'en-US',
-    page: 1
+    page: pageNumber
   };
-
-  $.getJSON(THE_MOVIE_DATABASE_ENDPOINT, params, displayMovies);
+  // done ensures movies are only displayed after properly stored - ask Jillian for more details
+  $.getJSON(THE_MOVIE_DATABASE_ENDPOINT, params, pushMoviesToStorage).done(displayFiveMovies);
 }
 
-function displayMovies (response) {
-  let movies = response.results;
-  //popularMovies.push(movies);
-  //console.log(popularMovies);
-  let start = 0;
-  let end = 5;
-  let fiveMoviesToDisplay = movies.slice(start, end);
-  const addMovieButton = `<button class="add-movie">Add to List</button>`;
-
-  function pushMoviesToStorage () {
-  	movies.forEach(function(movie) {
-  		popularMovies.push(movie);
-  	});
-  	console.log(popularMovies);
-  }
-  pushMoviesToStorage();
-
-  function displayFiveMovies () {
-    let output = '';
-    fiveMoviesToDisplay.forEach(function(movie) {
-    	let moviePoster = `${THE_MOVIE_DATABASE_IMAGE_BASEURL + THE_MOVIE_DATABASE_IMAGE_SIZE + movie["poster_path"]}`;
-    	let movieRating = movie["vote_average"];
-      output += `<li><img src=${moviePoster}><br>${movie.title}<br>${movieRating}<br>${addMovieButton}</li>`;
-    });
-    $('.movie-list').html(output);
-  }
-  displayFiveMovies();
-
-  function displayNextFiveMovies () {
-  	$('.next-button').on('click', function() {
-  		start += 5;
-	  	end += 5;
-	  	//this isn't working
-			displayFiveMovies();
-  	});
-  }
+function pushMoviesToStorage (response) {
+  movies = response.results;
+  movies.forEach(function(movie) {
+    popularMovies.push(movie);
+  });
+  console.log(popularMovies);
 }
 
-$('.load-movies').on('click', retrieveMovies);
+function displayFiveMovies () {
+  console.log(start + ' ' +  end);
+  let output = '';
+  popularMovies.slice(start, end).forEach(function(movie) {
+    let moviePoster = `${THE_MOVIE_DATABASE_IMAGE_BASEURL + THE_MOVIE_DATABASE_IMAGE_SIZE + movie["poster_path"]}`;
+    let movieRating = movie["vote_average"];
+    output += `<li><img src=${moviePoster}><br>${movie.title}<br>Rating: ${movieRating} / 10<br>${addMovieButton}</li>`;
+  });
+  $('.movie-list').html(output);
+}
+
+function displayNextFiveMovies () {
+  $('.next-button').on('click', function() {
+    if (end >= popularMovies.length) {
+      pageNumber++;
+      retrieveMovies(pageNumber);
+    }
+    start += resultsPerPage;
+    end += resultsPerPage;
+    displayFiveMovies();
+  });
+}
+
+function displayPrevFiveMovies () {
+  $('.prev-button').on('click', function() {
+    start -= resultsPerPage;
+    end -= resultsPerPage;
+    displayFiveMovies();
+  });
+}
 
 $(getDistance);
+$(retrieveFirstTwentyMovies);
+$(displayNextFiveMovies);
+$(displayPrevFiveMovies);
